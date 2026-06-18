0430914ff2b53bd700b4381770473a4c34af8556
mquartey-dev <mquartey@byupathway.edu>
2026-06-18 09:14:12 +0100

    Add Swimming activity class for tracking swimming exercises
    - Implemented Swimming class inheriting from Activity
    - Added constructor to initialize date, minutes, and laps
    - Defined GetDistance method to calculate distance based on laps
    - Implemented GetSpeed method to calculate speed in mph
    - Added GetPace method to calculate pace in minutes per mile

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Running runningAct = new Running("03 Nov 2022", 30, 3.0);
+        Cycling cyclingAct = new Cycling("04 Nov 2022", 45, 15.0);
+        Swimming swimmingAct = new Swimming("05 Nov 2022", 20, 40);
+
+        List<Activity> activities = new List<Activity>();
+        activities.Add(runningAct);
+        activities.Add(cyclingAct);
+        activities.Add(swimmingAct);
+
+        Console.WriteLine("Exercise Tracking Summary:");
+        Console.WriteLine("--------------------------");
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
